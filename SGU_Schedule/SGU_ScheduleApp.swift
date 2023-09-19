d7429551b91e78b9dbe7d47bc0122d6a5e94a34f
ArconRM <85395885+ArconRM@users.noreply.github.com>
2023-09-19 15:43:49 +0400

    Initial Commit

---
+++ SGU_Schedule/SGU_ScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SGU_ScheduleApp.swift
+//  SGU_Schedule
+//
+//  Created by Артемий on 19.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct SGU_ScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
